8fbe2928fe7f28c0397ec21ed4d6afd45a29a9aa
Jack <jpawlowicz81@gmail.com>
2022-06-02 20:41:32 +0200

    second commit

--- cookie-auto-clicker.py
+++ cookie-auto-clicker.py
@@ -11,12 +11,12 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
-
+time.sleep(1)
 english = driver.find_element_by_id("langSelect-EN")
 english.click()
 
 driver.get("https://orteil.dashnet.org/cookieclicker/")
-time.sleep(4)
+time.sleep(6)
 big_cookie = driver.find_element_by_xpath('//*[@id="bigCookie"]')
 
 
@@ -25,22 +25,24 @@ big_cookie = driver.find_element_by_xpath('//*[@id="bigCookie"]')
 store_elements = []
 store_price_elements = []
 for index in range(0,19):
-    store_element = driver.find_element_by_css_selector(f"#product{index}")
-    store_price_element = driver.find_element_by_css_selector(f"#productPrice{index}")
-    store_elements.append(store_element)
-    store_price_elements.append(store_price_element)
+    store_elements.append(driver.find_element_by_css_selector(f"#product{index}"))
+    store_price_elements.append(driver.find_element_by_css_selector(f"#productPrice{index}"))
 
 
-store_prices_texts = [element.text for element in store_price_elements if element.text != ""]
+prices_texts = [element.text for element in store_price_elements if element.text != ""]
 
-print(store_prices_texts)
-for index in range(0,len(store_prices_texts)):
-    upragades_and_prices_dict = {
-        index:{store_element[index]:store_price_elements[index]}
-    }
+upragades_and_prices_dict = {}
+for index in range(0,len(prices_texts)):
+    upragades_and_prices_dict[store_elements[index]] = prices_texts[index]
 
-for _ in range(0,1000):
-    big_cookie.click()
+print(upragades_and_prices_dict)
+
+cookies_count = int(driver.find_element_by_css_selector("div #cookies").text.split()[0])
+print(cookies_count)
+
+# for _ in range(0,200):
+#     big_cookie.click()
+
 
 
 time.sleep(100)
